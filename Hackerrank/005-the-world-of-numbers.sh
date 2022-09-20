8439af8b2466b2d1e2c0808786dcd8f00205ad7d
Shreyas Chavhan <shreyaschavhan4@gmail.com>
2022-09-20 12:30:27 +0530

    `The world of numbers`

---
+++ Hackerrank/005-the-world-of-numbers.sh
@@ -0,0 +1,9 @@
+# The World of Numbers: https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers
+
+read -s X;
+read -s Y;
+
+echo $(($X + $Y));
+echo $(($X - $Y));
+echo $(($X * $Y));
+echo $(($X / $Y));
